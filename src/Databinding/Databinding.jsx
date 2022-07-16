a965df575fae659bccc051f45f17e560ccdde2cc
Mattie <minhnguyen153216@gmail.com>
2022-07-16 17:06:15 +0700

    commit

--- src/Databinding/Databinding.jsx
+++ src/Databinding/Databinding.jsx
@@ -1,9 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Databinding extends Component {
-  render() {
+  userName = "qwead";
+
+  showInfo() {
+    return "hello";
+  }
+
+  renderCard = () => {
     return (
-      <div>Databinding</div>
-    )
+      <div>
+        userName: {this.userName}
+        <img src="https://picsum.photos/200/200" alt="" />
+      </div>
+    );
+  };
+
+  //phương thức render
+  render() {
+    //biến và hàm thì khi khai báo dùng var,let,const
+    const hoTen = "asd";
+
+    const tinhDiemTrungBinh = (diemToan, diemVan) => {
+      return (diemToan + diemVan) / 2;
+    };
+
+    return (
+      <div>
+        <div>Databinding</div>
+        <p>Họ Tên: {hoTen}</p>
+        <p>Điểm trung bình: {tinhDiemTrungBinh(8, 3)}</p>
+        <p>{this.showInfo()}</p>
+        <div>{this.renderCard()}</div>
+      </div>
+    );
   }
 }
